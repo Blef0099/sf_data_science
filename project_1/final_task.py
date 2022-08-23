8f6ef18e1ad71f4356aa6139e2840b017fc85ee6
Blef <d.ivanoff72@gmail.com>
2022-08-23 13:10:25 +0300

    Create new folder

---
+++ project_1/final_task.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+def random_predict(number:int=35) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 35.
+        l_range (int, optional): Нижняя граница генерируемого диапазона
+        up_range (int, optional): Верхняя граница генерируемого диапазона
+
+    Returns:
+        int: Число попыток
+    """
+
+    count = 0
+    low_range = 1 # нижняя граница диапазона
+    up_range = 101 # верхняя граница диапазона
+    while True:
+        count += 1
+        predict_number = np.random.randint(low_range, up_range) # предполагаемое число
+        if predict_number > number:
+            up_range = predict_number # понижаем верхнюю границу диапазона
+        elif predict_number < number:
+            low_range = predict_number # повышаем нижнюю границу диапазона
+        else:
+            break # выход из цикла, если угадали
+    return(count)
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+
+# RUN
+if __name__ == '__main__':
+    score_game(random_predict)
